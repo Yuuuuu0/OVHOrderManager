74135869f7ea1967fd830439703e4169c143a856
Yu <44354554+Yuuuuu0@users.noreply.github.com>
2024-11-20 20:40:44 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,12 @@
 # 设置虚拟环境路径
 VENV_DIR="venv"
 
+# 检查 .env 文件是否存在
+if [ ! -f ".env" ]; then
+    echo ".env 文件不存在，请复制.env.example设置你要抢购的相关信息，并保存为.env文件"
+    exit 1
+fi
+
 # 检查虚拟环境是否存在，如果不存在则创建并安装依赖
 if [ ! -d "$VENV_DIR" ]; then
     echo "虚拟环境不存在，正在创建..."
